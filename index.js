7f7fb63d52b8b68b9ce2ce52ef5d7905ee64b11b
Meraj Faizan <mdmaraj1500@gmail.com>
2023-03-17 14:32:27 +0600

    mongoDB connect and express installed, now it's ready for practic

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k5v5ibx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,8 +18,8 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
-    const productCollection = db.collection("product");
+    const db = client.db("moonTech");
+    const productCollection = db.collection("products");
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
